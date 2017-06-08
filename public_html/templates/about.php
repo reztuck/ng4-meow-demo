a428c750bdb2a17392930f5235dd12d5fa358621
Tucker <tucker@fellafox.com>
2017-06-08 11:45:07 -0600

    Added route, component and template for angular

---
+++ public_html/templates/about.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: primary
+ * Date: 6/8/17
+ * Time: 11:42 AM
+ */
---
+++ src/app/components/about.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by primary on 6/8/17.
+ */
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs";
+
+@Component({
+    templateUrl: "./templates/about.php"
+})
+
+export class AboutComponent {}
